7ef3611bc801e1223153c5bcca5b662e83a49e8e
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-04-13 12:05:04 -0300

    feat: install cors for connection frontend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,9 @@
 const app = require('./app')
-const PORT = process.env.PORT || 3001;
-
-
-
 const mongoose = require('mongoose');
+
+const PORT = 3001;
+
+
 
 const uri = 'mongodb://localhost:27017/myapp';
 mongoose.connect(uri)
