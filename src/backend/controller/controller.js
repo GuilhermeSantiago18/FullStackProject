b28d10cf09e6beb75f103822eb4fa9de922a0468
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-04-15 17:27:48 -0300

    refactor: only request and response

--- src/backend/controller/controller.js
+++ src/backend/controller/controller.js
@@ -1,33 +1,15 @@
-const meliService = require("../service/mercadoLivreService");
-const buscapeService = require("../service/buscapeService");
+const searchService = require("../service/service");
 
-
-const searchProducts = async (req,res) => {
+const searchProducts = async (req, res) => {
   const { company, product } = req.body;
-  const urlMeli = `https://lista.mercadolivre.com.br/${product}#D[A:${product}]`;
-  const urlBuscape = `https://www.buscape.com.br/${product}`;
-  let result;
-  switch (company) {
-    case "all":
-      const productsMeli = await meliService.getWebScrapMeli(urlMeli);
-      const productsBuscape = await buscapeService.getWebScrapBuscape(urlBuscape);
-      result = [...productsMeli, ...productsBuscape];
-      break;
-    case "meli":
-      result = await meliService.getWebScrapMeli(urlMeli);
-      break;
-    case "buscape":
-      result = await buscapeService.getWebScrapBuscape(urlBuscape);
-      break;
+  const { filter } = req.query;
+  try {
+    const resultProducts = await searchService.searchProducts(company, product, filter);
+    return res.status(201).json(resultProducts);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
   }
- const { filter } = req.query;
-const regex = new RegExp(filter, "i")
- result = result.filter((product) =>  regex.test(product.title))
-
-if (result) {
-  return res.status(201).json(result);
-}
-  return res.status(500).json({ error: "Internal server error" });
 };
 
 module.exports = {
