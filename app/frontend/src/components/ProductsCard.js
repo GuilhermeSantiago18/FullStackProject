6a58ba0de585f8e16803420aa41ab83f45b4474e
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-04-15 21:52:57 -0300

    style: implements first changes at inputs and buttons

--- app/frontend/src/components/ProductsCard.js
+++ app/frontend/src/components/ProductsCard.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Button, Stack, Typography } from "@mui/material";
 
-export default function ProductsCard({ title, price, image, link }) {
+export default function ProductsCard({ title, price, image, link, market }) {
   return (
     <>
-      <h3>{title}</h3>
-      <img src={image} alt={title} width="50px" />
-      <p>{price}</p>
-      <button>
-      <a href={link} target="_blank" rel="nofollow noreferrer">Buy</a>
-      </button>
+      <Stack
+        direction="column"
+        // justifyContent="center"
+        // alignItems="center"
+        spacing={2}
+      >
+        <Typography variant="subtitle1">{title}</Typography>
+        <img src={image} alt={title} width="150px" />
+        <Typography>
+          {Number(price.replace(".", "").replace(",", ".")).toLocaleString(
+            "pt-BR",
+            { style: "currency", currency: "BRL" }
+          )}
+        </Typography>
+        <Typography>{market}</Typography>
+      </Stack>
+      <Button variant="contained" href={`${link}`} target="_blank">
+        Ir a web
+      </Button>
     </>
   );
 }
--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { requestProductsURL } from "../service/api";
 import ProductsCard from "../components/ProductsCard";
+import { Button, TextField, Select, MenuItem, FormControl } from "@mui/material";
 
 function Home() {
   const [selectedCompany, setSelectedCompany] = useState("all");
@@ -27,42 +28,46 @@ function Home() {
       search: input,
     };
     const { data } = await requestProductsURL(dataset);
-    console.log(data)
+    console.log(data);
     setProducts(data);
   };
 
   return (
     <div>
-      <label htmlFor="select">Escolha uma opção:</label>
-      <select id="select" value={selectedCompany} onChange={handleChange}>
-        <option value="all">All</option>
-        <option value="buscape">Buscape</option>
-        <option value="meli">Mercado Livre</option>
-      </select>
-
-      <label htmlFor="select"></label>
-      <select
-        id="select"
+      <FormControl sx={{ width: '20ch' }}>
+      <Select
+        value={selectedCompany}
+        onChange={handleChange}
+      >
+        <MenuItem value='all'>All</MenuItem>
+        <MenuItem value='buscape'>Buscape</MenuItem>
+        <MenuItem value='meli'>Meli</MenuItem>
+      </Select>
+      </FormControl>
+      <FormControl sx={{ width: '25ch' }}>
+      <Select
         value={selectedProduct}
         onChange={handleChangeProduct}
       >
-        <option value="tv">TV</option>
-        <option value="geladeira">Refrigerator</option>
-        <option value="celular">Mobile</option>
-      </select>
+        <MenuItem value='tv'>TV</MenuItem>
+        <MenuItem value='geladeira'>Refrigerator</MenuItem>
+        <MenuItem value='celular'>Mobile</MenuItem>
+      </Select>
+      </FormControl>
+      <TextField id="outlined-basic" value={input}  onChange={handleChangeInput}/>
 
-      <input type="text" onChange={handleChangeInput} value={input}></input>
-
-      <button type="button" onClick={handleClick}>
+      <Button color="success" variant="contained" onClick={handleClick}>
         Search
-      </button>
-      {products.map(({ title, price, image, link }, index) => (
+      </Button>
+      
+      {products.map(({ title, price, image, link, market }, index) => (
         <ProductsCard
           key={index}
           title={title}
           price={price}
           image={image}
           link={link}
+          market={market}
         />
       ))}
     </div>
