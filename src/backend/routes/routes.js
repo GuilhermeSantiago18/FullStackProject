e9e68d13ae770c085b61161cb73b7e2bacf90533
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-04-16 18:06:42 -0300

    chore: update directory name, and recreate all files

--- src/backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const productsController = require('../controller/controller')
-
-
-const routes = express.Router();
-routes.post('/products/url', productsController.searchProducts);
-
-module.exports = routes;
