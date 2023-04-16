fb4fc9986589a1ef9a751e5264f04da8a08a4f18
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-04-16 17:17:54 -0300

    feat: implements isLoading and circular progress

--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { requestProductsURL } from "../service/api";
 import ProductsCard from "../components/ProductsCard";
-import { Button, TextField, Select, MenuItem, FormControl } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  Grid,
+  Stack,
+  Container,
+  CircularProgress,
+} from "@mui/material";
 
 function Home() {
   const [selectedCompany, setSelectedCompany] = useState("all");
   const [selectedProduct, setSelectedProduct] = useState("tv");
   const [input, setInput] = useState("");
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = ({ target }) => {
     setSelectedCompany(target.value);
@@ -22,55 +33,84 @@ function Home() {
   };
 
   const handleClick = async () => {
+    setIsLoading(true);
     const dataset = {
       company: selectedCompany,
       product: selectedProduct,
       search: input,
     };
     const { data } = await requestProductsURL(dataset);
-    console.log(data);
+
     setProducts(data);
+    setIsLoading(false);
   };
 
   return (
-    <div>
-      <FormControl sx={{ width: '20ch' }}>
-      <Select
-        value={selectedCompany}
-        onChange={handleChange}
+    <Container>
+      <Stack
+        spacing={1}
+        direction={{ xs: "column", md: "row" }}
+        justifyContent="center"
+        alignItems="center"
+        p={5}
       >
-        <MenuItem value='all'>All</MenuItem>
-        <MenuItem value='buscape'>Buscape</MenuItem>
-        <MenuItem value='meli'>Meli</MenuItem>
-      </Select>
-      </FormControl>
-      <FormControl sx={{ width: '25ch' }}>
-      <Select
-        value={selectedProduct}
-        onChange={handleChangeProduct}
-      >
-        <MenuItem value='tv'>TV</MenuItem>
-        <MenuItem value='geladeira'>Refrigerator</MenuItem>
-        <MenuItem value='celular'>Mobile</MenuItem>
-      </Select>
-      </FormControl>
-      <TextField id="outlined-basic" value={input}  onChange={handleChangeInput}/>
-
-      <Button color="success" variant="contained" onClick={handleClick}>
-        Search
-      </Button>
-      
-      {products.map(({ title, price, image, link, market }, index) => (
-        <ProductsCard
-          key={index}
-          title={title}
-          price={price}
-          image={image}
-          link={link}
-          market={market}
+        <FormControl sx={{ width: "100%" }}>
+          <Select value={selectedCompany} onChange={handleChange}>
+            <MenuItem value="all">Todas</MenuItem>
+            <MenuItem value="buscape">Buscape</MenuItem>
+            <MenuItem value="meli">Meli</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: "100%" }}>
+          <Select value={selectedProduct} onChange={handleChangeProduct}>
+            <MenuItem value="tv">TV</MenuItem>
+            <MenuItem value="geladeira">Geladeira</MenuItem>
+            <MenuItem value="celular">Celular</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          id="outlined-basic"
+          label="Filtro de pesquisa"
+          variant="outlined"
+          fullWidth
+          value={input}
+          onChange={handleChangeInput}
         />
-      ))}
-    </div>
+        <Button
+          color="success"
+          variant="contained"
+          fullWidth
+          onClick={handleClick}
+        >
+          Pesquisar
+        </Button>
+      </Stack>
+      {isLoading ? (
+        <Grid container p={2} justifyContent="center" alignItems="center">
+          <CircularProgress />
+        </Grid>
+      ) : (
+        <Grid
+          container
+          p={2}
+          rowGap={1}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ color: "black" }}
+        >
+          {products.map(({ title, price, image, link, market }, index) => (
+            <ProductsCard
+              key={index}
+              title={title}
+              price={price}
+              image={image}
+              link={link}
+              market={market}
+            />
+          ))}
+        </Grid>
+      )}
+    </Container>
   );
 }
 
